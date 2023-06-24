de45edd596055d095e9068c9d8a3bb21cce80187
Mahima <mahityagi1809@gmail.com>
2023-06-24 22:39:18 +0530

    Added the files of the project

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#element', {
+  strings: ['Thankyou for visiting here.', 'Click on sidebar icons.'],
+  typeSpeed: 50,
+});
+
+var typed2 = new Typed('#element2',{
+    strings: ['Contact us.','Contact us.'],
+    typeSpeed: 60,
+})
+var typed3 = new Typed('#element3',{
+    strings: ['Get to Know me more.','Information about me:'],
+    typeSpeed:40,
+})
